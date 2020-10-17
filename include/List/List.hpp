074f2a477963d9382d54a17bc8b286bfe5e99770
Hugo Majerczyk <Superseth794@gmail.com>
2020-10-17 09:16:25 +0200

    Add basis of list storage

---
+++ include/List/List.hpp
@@ -0,0 +1,18 @@
+//
+//  List.hpp
+//  mpt
+//
+//  Created by Jaraxus on 17/10/2020.
+//
+
+#ifndef List_h
+#define List_h
+
+namespace mpt {
+
+template <typename ...T>
+struct list {};
+
+} // mpt
+
+#endif /* List_h */
