e450c09e71006a52dcbf7be27660dde807f2f294
Hugo Majerczyk <Superseth794@gmail.com>
2020-10-23 16:20:01 +0200

    Add all_of trait

---
+++ include/utility/all_of.hpp
@@ -0,0 +1,32 @@
+//
+//  all_of.hpp
+//  mpt
+//
+//  Created by Jaraxus on 22/10/2020.
+//
+
+#ifndef all_of_h
+#define all_of_h
+
+#include <type_traits>
+
+# include "../Math/Number.hpp"
+
+namespace mpt {
+
+template <typename ...T>
+struct all_of {};
+
+template <typename T, typename ...XS>
+struct all_of<T, XS...> {
+    using value = std::conditional_t<T::value, typename all_of<XS...>::value, false_>;
+};
+
+template <typename T>
+struct all_of<T> {
+    using value = typename T::value;
+};
+
+}
+
+#endif /* all_of_h */
