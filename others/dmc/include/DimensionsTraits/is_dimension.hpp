4f146a5bcd37c3a523e39dbca823aa3a559904fc
Hugo Majerczyk <Superseth794@gmail.com>
2020-10-23 21:25:53 +0200

    Add is_dimension trait

---
+++ others/dmc/include/DimensionsTraits/is_dimension.hpp
@@ -0,0 +1,35 @@
+//
+//  is_dimension.hpp
+//  mpt
+//
+//  Created by Jaraxus on 22/10/2020.
+//
+
+#ifndef is_dimension_h
+#define is_dimension_h
+
+#include <type_traits>
+
+#include "is_single_dimension.hpp"
+#include "is_compound_dimension.hpp"
+#include "../Dimension.hpp"
+
+#include "../../../../include/Math/Number.hpp"
+#include "../../../../include/utility/all_of.hpp"
+#include "../../../../include/utility/or_.hpp"
+
+namespace dmc {
+
+template <typename ...T>
+struct is_dimension {
+    using value = std::conditional_t<
+                    mpt::all_of<
+                        typename mpt::or_<
+                            typename is_single_dimension<T>::value, typename is_compound_dimension<T>::value
+                        >::value...
+                    >::value::value, mpt::true_, mpt::false_>;
+};
+
+}
+
+#endif /* is_dimension_h */
