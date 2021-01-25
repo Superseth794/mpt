f26e608dd360057ebed6a0d93cd54b8b6159c60c
Hugo Majerczyk <Superseth794@gmail.com>
2021-01-25 20:13:00 +0100

    Add basis for writting compile-time unit tests

---
+++ tests/test.hpp
@@ -0,0 +1,17 @@
+//
+//  Test.hpp
+//  mpt
+//
+//  Created by Jaraxus on 25/01/2021.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#define MPT_ASSERT(...) static_assert(__VA_ARGS__::value, "")
+
+#define MPT_ASSERT_NOT(...) static_assert(!__VA_ARGS__::value, "")
+
+#define TEST_CASE(x) void test_case_##x()
+
+#endif /* Test_hpp */
